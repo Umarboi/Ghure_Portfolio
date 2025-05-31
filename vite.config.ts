e347c89d59784396f5334ab442c2576c120dfae0
Umarboi <ghuremohdumar@gmail.com>
2025-06-01 03:35:48 +0530

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+const base = '/Ghure_Portfolio/';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base,
   server: {
     host: "::",
     port: 8080,
